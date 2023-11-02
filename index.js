b948ffa668cf8e332d7d422f2f4e50fb383c7b20
RyokuZero <antonio_carlos02@hotmail.com>
2023-11-02 07:14:51 -0300

    desafio completo

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
-console.log("Olá mundão")
-console.log("teste 2")
+
+let name = "Brabo"
+let nivel = 100000
+
+switch (true){
+      case (nivel <= 1000):
+      console.log("O Herói de nome " + name + " está no nível de Ferro")
+      break
+      
+      case (nivel >= 1001 && nivel <= 2000):
+      console.log("O Herói de nome " + name + " está no nível de Bronze")
+      break
+      
+      case (nivel >= 2001 && nivel <= 5000):
+      console.log("O Herói de nome " + name + " está no nível de Prata")
+      break
+      
+      case (nivel >= 5001 && nivel <= 7000):
+      console.log("O Herói de nome " + name + " está no nível de Ouro")
+      break
+      
+      case (nivel >= 7001 && nivel <= 8000):
+      console.log("O Herói de nome " + name + " está no nível de Platina")
+      break
+      
+      case (nivel >= 8001 && nivel <= 9000):
+      console.log("O Herói de nome " + name + " está no nível Ascendente")
+      break
+      
+      case (nivel >= 9001 && nivel <= 10000):
+      console.log("O Herói de nome " + name + " está no nível Imortal")
+      break
+      
+      case (nivel > 10001):
+      console.log("O Herói de nome " + name + " está no nível Radical")
+      break
+      
+      default:
+        console.log("Por favor informar dados numericos apenas");
+}
